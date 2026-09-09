4ebb3a5b91228e33b6cafc95d6ac7f5db9035fc6
letov7482-ui <letov7482@gmail.com>
2026-09-09 16:07:27 +0300

    Add AdaptivePerformanceEngine to FluxClient

--- src/client/java/com/flux/FluxClient.java
+++ src/client/java/com/flux/FluxClient.java
@@ -1,10 +1,11 @@
 package com.flux;
 
 import com.flux.gui.FluxScreen;
+import com.flux.performance.AdaptivePerformanceEngine;
 import com.flux.performance.FramePacingEngine;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 import org.lwjgl.glfw.GLFW;
@@ -17,6 +18,9 @@ public class FluxClient implements ClientModInitializer {
 
     private static final FramePacingEngine FRAME_PACING =
             new FramePacingEngine();
+
+    private static final AdaptivePerformanceEngine ADAPTIVE_PERFORMANCE =
+            new AdaptivePerformanceEngine();
 
     @Override
     public void onInitializeClient() {
@@ -31,15 +35,23 @@ public class FluxClient implements ClientModInitializer {
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
 
+            ADAPTIVE_PERFORMANCE.tick(FRAME_PACING);
+
             if (openGuiKey.consumeClick()) {
                 client.setScreen(new FluxScreen(client.screen));
             }
         });
 
-        System.out.println("Flux Performance Engine initialized.");
+        System.out.println(
+                "Flux Performance Engine initialized."
+        );
     }
 
     public static FramePacingEngine getFramePacing() {
         return FRAME_PACING;
+    }
+
+    public static AdaptivePerformanceEngine getAdaptivePerformance() {
+        return ADAPTIVE_PERFORMANCE;
     }
 }
